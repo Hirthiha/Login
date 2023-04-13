2e4ae282bf99e541c4a3b66bf1067850037d1067
hirthiha <hirthiha.doodleblue@gmail.com>
2023-04-13 12:24:33 +0530

    npm add

--- realm/realmconfig.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
-function Task(){
-    AsyncStorage.multiSet('collection',[
-        
-      ])
-}
-export default Task;
